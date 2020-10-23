93de656314988e562e8b31a0cf4c6a6ac4d8fcea
Rachel Xiao <rachelxiao907@gmail.com>
2020-10-23 12:08:20 -0400

    Added ArrayOps.largest()

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,12 @@ public class ArrayOps {
     }
     return sum;
   }
+
+  public static int largest(int[] arr) {
+    int max = 0;
+    for (int i = 0; i < arr.length; i++) {
+      max = Math.max(max, arr[i]);
+    }
+    return max;
+  }
 }
