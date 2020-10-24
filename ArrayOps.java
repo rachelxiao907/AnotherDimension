0ea8dd5584c04cd428e4b7cafa2f441d53a2e75c
Rachel Xiao <rachelxiao907@gmail.com>
2020-10-24 13:54:39 -0400

    Modified sum() for a 2D array

--- ArrayOps.java
+++ ArrayOps.java
@@ -32,12 +32,6 @@ public class ArrayOps {
   }
 
   public static int sum(int[][] arr) {
-    int sum = 0;
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr[i].length; j++) {
-        sum += arr[i][j];
-      }
-    }
-    return sum;
+    return sum(sumRows(arr));
   }
 }
